84d07996af1aec93476c36d4322867979bd844a9
Ayush Saksena <137744036+ayushsaksena30@users.noreply.github.com>
2025-06-08 17:09:10 +0530

    Added: Colors, font, theme, icon

--- app/src/main/java/com/tpc/nudj/MainActivity.kt
+++ app/src/main/java/com/tpc/nudj/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,13 +44,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .fillMaxSize()
+            .padding(WindowInsets.systemBars.asPaddingValues())
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     NudjTheme {
-        Greeting("Android")
+        Greeting("this is Nudj Preview")
     }
 }
--- app/src/main/java/com/tpc/nudj/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/tpc/nudj/ui/screens/auth/LoginScreen.kt
@@ -14,7 +14,6 @@ private fun LoginScreenAbstract(){
 
 }
 
-@Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun LoginScreenPreview(){
