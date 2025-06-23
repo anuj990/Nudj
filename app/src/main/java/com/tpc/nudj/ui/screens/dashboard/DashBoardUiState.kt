435974cef7406d3113a5acaae8bc1ca0a6e06447
nirvan73 <nirvanj73@gmail.com>
2025-06-23 13:14:40 +0530

    Added animated bottom navigation with HorizontalPager

---
+++ app/src/main/java/com/tpc/nudj/ui/screens/dashboard/DashBoardUiState.kt
@@ -0,0 +1,5 @@
+package com.tpc.nudj.ui.screens.dashboard
+
+data class DashBoardUiState(
+    val selectedPage: Int = 0,
+)
--- app/src/main/java/com/tpc/nudj/ui/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/tpc/nudj/ui/screens/dashboard/DashboardScreen.kt
@@ -1,17 +1,208 @@
 package com.tpc.nudj.ui.screens.dashboard
 
+import android.content.res.Configuration
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.getValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import com.tpc.nudj.ui.theme.NudjTheme
+import kotlinx.coroutines.delay
+import com.tpc.nudj.R
+import com.tpc.nudj.ui.theme.EditTextBackgroundColorLight
+import com.tpc.nudj.ui.theme.LocalAppColors
+
+
+enum class BottomNavScreen(@StringRes val title: Int, @DrawableRes val icon: Int) {
+    Home(title = R.string.home, R.drawable.home_image),
+    Clubs(title = R.string.my_clubs, R.drawable.club_image),
+    Rsvp(title = R.string.rsvp, R.drawable.rsvp_image),
+    Profile(title = R.string.profile, R.drawable.profile_image)
+}
 
 @Composable
-fun DashboardScreen(){
-    Scaffold { padding->
-        Box(modifier = Modifier.padding(padding)){
-            Text("Welcome to Nudj!!")
+fun DashboardScreen(viewModel: DashBoardViewModel = viewModel()) {
+    val uiState by viewModel.dashBoardUiState.collectAsState()
+    val destination = listOf(
+        BottomNavScreen.Home,
+        BottomNavScreen.Clubs,
+        BottomNavScreen.Rsvp,
+        BottomNavScreen.Profile
+    )
+    val pagerState = rememberPagerState(initialPage = uiState.selectedPage) { destination.size }
+    LaunchedEffect(pagerState.currentPage) {
+        if (uiState.selectedPage != pagerState.currentPage) {
+            delay(100)
+            viewModel.onScreenClicked(pagerState.currentPage)
         }
+    }
+    LaunchedEffect(uiState.selectedPage) {
+        if (pagerState.currentPage != uiState.selectedPage) {
+            delay(300)
+            pagerState.animateScrollToPage(uiState.selectedPage)
+        }
+    }
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                uiState = uiState,
+                screens = destination,
+                onScreenChange = { index -> viewModel.onScreenClicked(index) }
+            )
+        }
+    ) { padding ->
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) { page ->
+            val screen = destination[page]
+                NavScreens(screen)
+        }
+    }
+}
+
+@Composable
+fun NavScreens(
+    screen: BottomNavScreen
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LocalAppColors.current.background),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Text(
+            text = stringResource(screen.title),
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    screens: List<BottomNavScreen> = emptyList<BottomNavScreen>(),
+    uiState: DashBoardUiState,
+    onScreenChange: (Int) -> Unit,
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(85.dp)
+    ) {
+        val positionWidth = this.maxWidth / screens.size
+        val slidingEffect by animateDpAsState(
+            targetValue = (uiState.selectedPage * positionWidth.value).dp,
+            animationSpec = tween(durationMillis = 300)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(EditTextBackgroundColorLight)
+        )
+        Box(
+            modifier = Modifier
+                .padding(start = 16.dp, top = 6.dp)
+                .offset(x = slidingEffect)
+                .width((positionWidth.value - 32).dp)
+                .height(34.dp)
+                .shadow(8.dp, shape = RoundedCornerShape(20.dp))
+                .clip(RoundedCornerShape(20.dp))
+                .background(LocalAppColors.current.landingPageBackground)
+                .padding(end = 20.dp)
+                .align(Alignment.TopStart),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 8.dp)
+                .background(Color.Transparent),
+        ) {
+            screens.forEachIndexed { index, dest ->
+                val animateIconSize by animateDpAsState(
+                    targetValue = if (uiState.selectedPage == index) 32.dp else 30.dp,
+                    animationSpec = tween(durationMillis = 300)
+                )
+                val slidingEffectY by animateDpAsState(
+                    targetValue = if (uiState.selectedPage == index) -4.dp else 0.dp,
+                    animationSpec = tween(durationMillis = 300)
+                )
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = dest.icon),
+                            contentDescription = stringResource(dest.title),
+                            tint = LocalAppColors.current.landingPageAppTitle,
+                            modifier = Modifier
+                                .size(animateIconSize)
+                                .offset(y = slidingEffectY)
+                        )
+                    },
+                    selected = uiState.selectedPage == index,
+                    onClick = {
+                        onScreenChange(index)
+                    },
+                    alwaysShowLabel = true,
+                    label = {
+                        Text(
+                            text = stringResource(dest.title),
+                            color = LocalAppColors.current.landingPageAppTitle,
+                            style = MaterialTheme.typography.labelMedium.copy(
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = Color.Transparent
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DashboardScreenPreview() {
+    NudjTheme {
+        DashboardScreen()
     }
 }
